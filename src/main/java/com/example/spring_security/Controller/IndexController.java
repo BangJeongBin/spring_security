b2ea62026a824e381a395460cd0b5d1b393bafc7
41817 <bangjungbin@gmail.com>
2025-03-20 17:01:26 +0900

    Initial commit

---
+++ src/main/java/com/example/spring_security/Controller/IndexController.java
@@ -0,0 +1,25 @@
+package com.example.spring_security.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello World";
+    }
+
+    // User, Admin 접근 가능
+    @GetMapping("/user")
+    public String user() {
+        return "Hello user";
+    }
+
+    // Admin만 접근 가능
+    @GetMapping("/admin")
+    public String admin() {
+        return "Hello admin";
+    }
+}
