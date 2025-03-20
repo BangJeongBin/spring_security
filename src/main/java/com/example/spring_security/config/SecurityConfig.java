d988fef934474ea185c61447561aa3523cdf73d6
41817 <bangjungbin@gmail.com>
2025-03-20 17:47:41 +0900

    ADMIN 계정에 USER 권한 추가

--- src/main/java/com/example/spring_security/config/SecurityConfig.java
+++ src/main/java/com/example/spring_security/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
         UserDetails admin = User.withDefaultPasswordEncoder()
                 .username("admin")
                 .password("password")
-                .roles("ADMIN")
+                .roles("ADMIN", "USER") // ADMIN, USER 권한 가짐
                 .build();
 
         return new InMemoryUserDetailsManager(user, admin);
